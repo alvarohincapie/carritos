6385eec6a95e4c7935d928c41253d9704be2f70b
alvarohincapie@gmail.com <alvarohincapie@gmail.com@cf57b016-9254-9918-2bcb-82f70f16b22d>
2014-04-20 23:28:10 +0000

    x

--- trunk/siscarLogic/src/geniar/siscar/logic/parameters/services/FindParameterService.java
+++
@@ -1,9 +0,0 @@
-package geniar.siscar.logic.parameters.services;
-
-import gwork.exception.GWorkException;
-
-public interface FindParameterService {
-	
-	public Float findParameterByName(String nombreParametro) throws GWorkException;
-	public Float findParameterById(Long idParametro)throws GWorkException;
-}
--- trunk/siscarLogic/src/geniar/siscar/logic/parameters/services/WorkingDaysService.java
+++
@@ -1,9 +0,0 @@
-package geniar.siscar.logic.parameters.services;
-
-import gwork.exception.GWorkException;
-
-public interface WorkingDaysService {
-	
-	void findWorkingDays()throws GWorkException;
-
-}
